1a5c148f06041406138090b5637853d8d765cc80
Ashwani Ahlawat <49407895+AshuAhlawat@users.noreply.github.com>
2022-07-18 12:55:09 +0530

    basic models created

--- Shop/urls.py
+++ Shop/urls.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf.urls.static import static
+from django.conf import settings
+
+from django.shortcuts import redirect
+
+default = lambda request: redirect('/customer/home')
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('customer/', include("customer.urls")),
-    path('supplier/', include("supplier.urls"))
+    path('supplier/', include("supplier.urls")),
+    path('', default)
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- supplier/models.py
+++ supplier/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(default="untitled", max_length=100)
+    desciption = models.CharField(max_length=1000)
+    category = models.CharField(default="unclassified",max_length=30)
+
+    image = models.ImageField(upload_to="product_images/")
+
+    price = models.PositiveIntegerField(default=100)
+    discount = models.FloatField(default=0.0)
+
+    pack_of = models.PositiveIntegerField(default=1)
+    units = models.PositiveIntegerField(default=1)
+    in_stock = models.BooleanField(default=True)
+
+    supplier_id = models.IntegerField(default=1)
